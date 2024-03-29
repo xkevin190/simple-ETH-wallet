af864512ebaf04e9017cc369f8308c68ead0ccde
xkevin190 <kevinvelasco193@gmail.com>
2024-03-29 10:47:56 +0100

    Initial Commit

---
+++ singleEthWallet/singleEthWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  singleEthWalletApp.swift
+//  singleEthWallet
+//
+//  Created by Kevin Velasco on 29/3/24.
+//
+
+import SwiftUI
+
+@main
+struct singleEthWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
